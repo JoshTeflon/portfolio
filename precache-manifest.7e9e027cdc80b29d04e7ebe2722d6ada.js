7e5b51fd5da6c02a6be35281157a2ffdcf37ee8f
Josh Teflon <jenikele21h@gmail.com>
2020-05-02 04:24:08 +0100

    Updates

---
+++ precache-manifest.7e9e027cdc80b29d04e7ebe2722d6ada.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782cc538f14ccc3bd1ef229669232300",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1545af65e2ae08fabd9a",
+    "url": "/portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "e2c6838c5f6383ae2673",
+    "url": "/portfolio/static/css/main.5aa123ff.chunk.css"
+  },
+  {
+    "revision": "1545af65e2ae08fabd9a",
+    "url": "/portfolio/static/js/2.9c557cef.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/portfolio/static/js/2.9c557cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c6838c5f6383ae2673",
+    "url": "/portfolio/static/js/main.6b2b4227.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "ca7a30472cf81a41936c9289921dc583",
+    "url": "/portfolio/static/media/Freelancer.ca7a3047.png"
+  },
+  {
+    "revision": "ef5e0c7ffcd1d5b3f6e673d92aeec666",
+    "url": "/portfolio/static/media/Gmail.ef5e0c7f.png"
+  },
+  {
+    "revision": "24c91ec1ece58e9bba7a9070d12e02f2",
+    "url": "/portfolio/static/media/Teflon.24c91ec1.png"
+  },
+  {
+    "revision": "3d4af2092b3ed3a9b673180daa92b20e",
+    "url": "/portfolio/static/media/aboutJosh.3d4af209.JPG"
+  }
+]);
